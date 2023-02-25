819e75c35730ee3b6752d4812cc564efb6ff681e
andyluo03 <76852584+andyluo03@users.noreply.github.com>
2023-02-25 03:19:28 -0600

    IT WORKS

--- src/crud/create.py
+++ src/crud/create.py
@@ -3,12 +3,17 @@ import requests
 import json
 from __main__ import app
 
+CONFIG = dict(json.load(open("config.json")))
+HEADERS = CONFIG["HEADERS"]
+BASE_URL = CONFIG["BASE_URL"]
+
 @app.route("/create")
 def create():
     body = json.loads(request.data, strict=False)
     channel = body["channel"]
-    data = body["data"]
+    payload = body["data"]
 
-    
+    x = requests.post(f'{BASE_URL}/channels/{channel}/messages', data=json.dumps(payload), headers=HEADERS)
+    print(x.text)
 
     return "Success!"
