33ecd633716d574f6a37948f896b53953cada7e1
andyluo03 <76852584+andyluo03@users.noreply.github.com>
2023-02-25 03:42:43 -0600

    CUD

--- src/crud/delete.py
+++ src/crud/delete.py
@@ -1,7 +1,19 @@
 from flask import Flask, request
 import requests
+import json
 from __main__ import app
+
+CONFIG = dict(json.load(open("config.json")))
+HEADERS = CONFIG["HEADERS"]
+BASE_URL = CONFIG["BASE_URL"]
 
 @app.route("/delete")
 def delete():
-    return "test"
+    body = json.loads(request.data, strict=False)
+    channel = body["channel"]
+    message_id = body["message_id"]
+
+    x = requests.delete(f'{BASE_URL}/channels/{channel}/messages/{message_id}', headers=HEADERS)
+    print(x)
+
+    return "Success!"
--- src/crud/update.py
+++ src/crud/update.py
@@ -1,7 +1,20 @@
 from flask import Flask, request
 import requests
+import json
 from __main__ import app
+
+CONFIG = dict(json.load(open("config.json")))
+HEADERS = CONFIG["HEADERS"]
+BASE_URL = CONFIG["BASE_URL"]
 
 @app.route("/update")
 def update():
-    return "test"
+    body = json.loads(request.data, strict=False)
+    channel = body["channel"]
+    message_id = body["message_id"]
+    payload = body["data"]
+
+    x = requests.patch(f'{BASE_URL}/channels/{channel}/messages/{message_id}', data=json.dumps(payload), headers=HEADERS)
+    print(x)
+
+    return "Success!"
