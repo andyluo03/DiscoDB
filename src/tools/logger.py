2a3bb34b5961c73893a48ef540887c8675316636
andyluo03 <76852584+andyluo03@users.noreply.github.com>
2023-02-26 00:40:20 -0600

    RATE LIMIT LOGGER

--- src/tools/logger.py
+++ src/tools/logger.py
@@ -1,16 +1,21 @@
 import requests
 import time
 import json
+from ratelimit import sleep_and_retry, limits
 
 CONFIG = dict(json.load(open("config.json")))
 HEADERS = CONFIG["HEADERS"]
 BASE_URL = CONFIG["BASE_URL"]
 LOG_CHANNEL = CONFIG["LOG_CHANNEL_ID"]
 
+@sleep_and_retry
+@limits(calls=5, time=1)
 def log_request(unprocessed_request):
     log = {"content": '{"type": "' + unprocessed_request.method + '", "time": "' + str(time.time()) + '"}'}
     requests.post(f'{BASE_URL}/channels/{LOG_CHANNEL}/messages', data=json.dumps(log),headers=HEADERS)
 
+@sleep_and_retry
+@limits(calls=5, time=1)
 def log_failure(status_code):
     log = {"content": '{"error": "'+ status_code +'", "time": "' + str(time.time()) + '"}'}
     requests.post(f'{BASE_URL}/channels/{LOG_CHANNEL}/messages', data=json.dumps(log),headers=HEADERS)
