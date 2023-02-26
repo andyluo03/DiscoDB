fa9bc7507cb719645414a46026ad5bde717f7c0c
andyluo03 <76852584+andyluo03@users.noreply.github.com>
2023-02-26 01:11:28 -0600

    WE WILL MAKE IT OUT

--- src/resources/login.py
+++ src/resources/login.py
@@ -2,15 +2,19 @@ from flask import Flask, request
 import json
 import bcrypt
 import jwt
-from __main__ import app
-from tools import discord_crud
 import requests
 from base64 import b64decode
+from ratelimit import sleep_and_retry, limits
+
+from __main__ import app
+from tools import discord_crud
 
 BASE_URL = discord_crud.BASE_URL
 HEADERS = discord_crud.HEADERS
 USERS_CHANNEL_ID = discord_crud.USERS_CHANNEL_ID
 
+@sleep_and_retry
+@limits(calls=2, period=1)
 def query_user(user: str):
     parameters = {"limit":100}
     message_list = requests.get(f'{BASE_URL}/channels/{USERS_CHANNEL_ID}/messages', params=parameters, headers=HEADERS)
